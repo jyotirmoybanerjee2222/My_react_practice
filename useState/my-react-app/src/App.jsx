377e64e3b984ac83df6012d56068cf6dcf91b307
jyotirmoybanerjee2222 <jyotirmoybanerjee0511@gmail.com>
2024-12-02 11:47:05 +0530

    UseState

--- useState/my-react-app/src/App.jsx
+++ useState/my-react-app/src/App.jsx
@@ -1,8 +1,12 @@
 import MyComponent from "./MyComponent.jsx";
+import Counter from "./Counter.jsx";
 function App(){
  return(
- <MyComponent>
-      <MyComponent/>
- </MyComponent>)
+//  <MyComponent>
+//       <MyComponent/>
+//  </MyComponent>
+     < Counter />
+)
+ 
 }
 export default App
