7875d772d368fd741fc3fd0db331ab4b085c09c3
jyotirmoybanerjee2222 <jyotirmoybanerjee0511@gmail.com>
2024-12-05 12:32:40 +0530

    To-Do-List

--- To-Do-App/vite-project/src/ToDoList.jsx
+++ To-Do-App/vite-project/src/ToDoList.jsx
@@ -7,22 +7,34 @@ function ToDoList(){
         setNewTask(event.target.value);
     }
     function addTask(){
-
+        if(newTask.trim() !== ""){
+            setTask(t => [...t,newTask]);
+            setNewTask("");
+        }        
     }
     function deleteTask(index){
-
+       const updateTasks = task.filter((element,i) => i !== index);
+       setTask(updateTasks);
     }
     function moveTaskUp(index)
     {
-
+        if (index > 0){
+            const updateTasks =[ ...task];
+            [updateTasks[index],updateTasks[index-1]]=[updateTasks[index-1],updateTasks[index]];
+            setTask(updateTasks);
+        }
     }
     function moveTaskDown(index)
     {
-
+        if (index < task.length-1){
+            const updateTasks =[ ...task];
+            [updateTasks[index],updateTasks[index+1]]=[updateTasks[index+1],updateTasks[index]];
+            setTask(updateTasks);
+        }
     }
     return(
-        <div className="to-do-List">
-            <h1>To-Do_List</h1>
+        <div className="to-do-list">
+            <h1>To-Do-List</h1>
             <div>
                 <input  type ="text"  placeholder="Enter a task.."
                  value={newTask}
@@ -36,7 +48,7 @@ function ToDoList(){
                         <span className="text">{task}</span>
                         <button className="delete-button" onClick={() => deleteTask(index)} >Delete</button>
                         <button className="move-button" onClick={() => moveTaskUp(index)} >UP</button>
-                        <button className="move-button" onClick={() => moveTaskDown(index)} >DOwn</button>
+                        <button className="move-button" onClick={() => moveTaskDown(index)} >Down</button>
                         
                     </li>
                 )}
